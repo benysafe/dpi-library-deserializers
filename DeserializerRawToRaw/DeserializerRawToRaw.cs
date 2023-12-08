1e44aa58044e42f608218e91d7a3c623c71fdf34
Benigno Eduardo Santos Fernández <benignosantos91@gmail.com>
2023-12-08 14:20:53 -0500

    Sin cambios

---
+++ DeserializerRawToRaw/DeserializerRawToRaw.cs
@@ -0,0 +1,80 @@
+﻿using InterfaceLibraryDeserializer;
+using InterfaceLibraryProcessor;
+using InterfaceLibraryConfigurator;
+using Definitions;
+using System;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Threading;
+using InterfaceLibraryLogger;
+using NLog;
+using System.Text;
+
+namespace DeserializerRawToRaw
+{
+
+    public class DeserializerRawToRaw : IDeserializer
+    {
+        private IProcessor _Processor;
+        private Logger _logger;
+        private string _id;
+        private IConfigurator _configurator;
+
+        public void init(string id, IConfigurator configurator, IGenericLogger logger)
+        {
+            try
+            {
+                _id = id;
+                _configurator = configurator;
+                _logger = (Logger)logger.init("DeserializerRawToRaw");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                throw ex;
+            }
+        }
+
+        public void addProcessor(IProcessor processor)
+        {
+            try
+            {
+                _Processor = processor;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                throw ex;
+            }
+        }
+
+        public bool deserialize(object payload, Dictionary<string, object> metadata)
+        {
+            try
+            {
+                _logger.Trace("Inicio");
+                string strPayload = Encoding.UTF8.GetString((byte[])payload);
+                string strMetadata = null;
+                if (metadata != null)
+                {
+                    strMetadata = System.Text.Json.JsonSerializer.Serialize<Dictionary<string, object>>(metadata);
+                }
+                _logger.Debug("  Dato '{0}'      Metadato  '{1}'", strPayload, strMetadata);
+
+                if (!this._Processor.proccess((object)payload, (object)strMetadata))
+                {
+                    _logger.Error($"No se ejecuto el procesamiento del mensaje: {strPayload}");
+                    return false;
+                }
+                _logger.Trace("Fin");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                return false;
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DeserializerToDictionary/DeserializerJsonToDictionary.cs
@@ -0,0 +1,80 @@
+﻿using InterfaceLibraryDeserializer;
+using InterfaceLibraryProcessor;
+using InterfaceLibraryConfigurator;
+using Definitions;
+using System;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Threading;
+using InterfaceLibraryLogger;
+using NLog;
+using System.Text;
+
+namespace DeserializerJsonToDictionary
+{
+    public class DeserializerJsonToDictionary : IDeserializer
+    {
+        private IProcessor _Processor;
+        private Logger _logger;
+        private string _id;
+        IConfigurator _configurator;
+
+        public void init(string id, IConfigurator configurator, IGenericLogger logger)
+        {
+            try
+            {
+                _id = id;
+                _configurator = configurator;
+                _logger = (Logger)logger.init("DeserializerJsonToDictionary");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                throw ex;
+            }
+        }
+
+        public void addProcessor(IProcessor processor)
+        {
+            try
+            {
+                _Processor = processor;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                throw ex;
+            }
+        }
+
+        public bool deserialize(object payload, Dictionary<string, object> metadata)
+        {
+            try
+            {
+                _logger.Trace("Inicio");
+                string strPayload = Encoding.UTF8.GetString((byte[])payload);    
+                Dictionary<string, object> dicPayload = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(strPayload);
+                string strMetadata = null;
+                if (metadata != null)
+                {
+                    strMetadata = System.Text.Json.JsonSerializer.Serialize<Dictionary<string, object>>(metadata);
+                }
+                _logger.Debug("  Dato '{0}'      Metadato  '{1}'", strPayload, strMetadata);
+
+                if (!_Processor.proccess(dicPayload, metadata))
+                {
+                    _logger.Error($"No se ejecuto el procesamiento del mensaje: {strPayload}");
+                    return false;
+                }
+                _logger.Trace("Fin");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());
+                return false;
+                throw ex;
+            }
+        }
+    }
+}
